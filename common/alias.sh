8f1cc7cd5492d620dcb28c11b626eb4f2a8629c5
kathmandu777 <mahhakatomanato@gmail.com>
2022-01-27 15:11:15 +0900

    update alias

--- common/alias.sh
+++ common/alias.sh
@@ -1,20 +1,35 @@
+# misc
 alias so='source'
+alias c='clear'
+alias vs='code .'
 
+# cd
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
+# ls
+alias ls='ls -F --color=auto'
+alias lsl='ls -lh --color=auto'
+alias ll='ls -lh --color=auto'
+alias la='ls -aF --color=auto'
+alias lsa='ls -aF --color=auto'
+alias lsla='ls -lah --color=auto'
+alias lsal='ls -alh --color=auto'
+
+# python
 alias py='python3'
 alias python='python3'
 alias pip='pip3'
+alias activate='source venv/bin/activate'
 
-alias vs='code .'
-
+# dir path
 alias p='cd $HOME/projects'
+alias dot='cd $HOME/dotfiles'
 
-alias c='clear'
-
+# docker
 alias d='docker'
 
-# python venv
-alias activate='source venv/bin/activate'
+# apt
+alias api='sudo apt install'
+alias apu='sudo apt update'
